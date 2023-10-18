82030009ebb83f0d6d301dd146ceed3b7fb458c2
somtola <somtola.smt@gmail.com>
2023-10-18 00:44:05 -0700

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +6,4 @@ declare var $:any;
 })
 export class AppComponent {
   title = 'first-project';
-
-  doempty:any;
-  ngOnIt() {
-    this.doempty="dsakfklkjasjlfkdja";
-  }
-  openModal(){
-    console.log()
-    this.doempty="dsakfklkjasjlfkdja";
-    $('#myModal').modal('show');
-  }
 }
